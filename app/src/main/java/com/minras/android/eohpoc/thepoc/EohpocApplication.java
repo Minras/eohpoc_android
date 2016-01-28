d6e57bd3c186d927110856ab31b9a87b71d3824e
minras <andrey.shchurkov@gmail.com>
2016-01-28 17:03:56 +0100

    Minor cleanup

--- app/src/main/java/com/minras/android/eohpoc/thepoc/EohpocApplication.java
+++ app/src/main/java/com/minras/android/eohpoc/thepoc/EohpocApplication.java
@@ -2,13 +2,8 @@ package com.minras.android.eohpoc.thepoc;
 
 import android.app.Application;
 
-import com.estimote.sdk.BeaconManager;
-
 
 public class EohpocApplication extends Application {
-    private BeaconManager beaconManager;
-    private String defaultUuid = "B9407F30-F5F8-466E-AFF9-25556B57FE6D";
-
     @Override
     public void onCreate() {
         super.onCreate();
